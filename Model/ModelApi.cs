304c360c046abe8a6adb5e5999f93ad07aafbc01
Piotr Zuchowski <piterowski2880@gmail.com>
2022-04-27 21:07:44 +0200

    Presetnation basic classes

---
+++ Model/ModelApi.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Logic;
+
+namespace Model
+{
+    public  abstract class ModelApi
+    {
+
+        public static ModelApi CreateLayer(Logic.LogicApi Lapi = default(LogicApi))
+        {
+            return new Model(Lapi == null ? LogicApi.CreateLayer() : Lapi);
+        }
+
+
+
+        // implementacja Api
+        private class Model : ModelApi
+        {
+            private readonly LogicApi logapi;
+            public Model(LogicApi logika)
+            {
+                logapi = logika;
+            }
+
+
+
+        }
+    }
+}
---
+++ ViewModel/ViewModelApi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Model;
+
+namespace ViewModel
+{
+    public abstract class ViewModelApi
+    {
+        public static ViewModelApi CreateLayer(ModelApi Mapi = default(ModelApi))
+        {
+            return new ViewModel(Mapi == null ? ModelApi.CreateLayer() : Mapi);
+        }
+
+        // implementacja Api
+        private class ViewModel : ViewModelApi
+        {
+            private readonly ModelApi _model;
+
+            public ViewModel(ModelApi MApi)
+            {
+                _model = MApi;
+            }
+        }
+    }
+}
