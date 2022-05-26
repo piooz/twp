048d4c681fbe6688eb369613fe619018db136402
wikslu <wiktoria.sluzewska@gmail.com>
2022-05-26 18:01:17 +0200

    moving stuff from logic to data

---
+++ Data/Ball.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace Data
+{
+    public class Ball : INotifyPropertyChanged
+    {
+
+        public double X;
+        public double Y;
+        public double R;
+
+        public double VelX;
+        public double VelY;
+        public double mass;
+
+        public Ball(double x, double y, double r, double _mass)
+        {
+            this.X = x;
+            this.Y = y;
+            this.R = r;
+/*            this.VelX = vx;
+            this.VelY = vy;*/
+            this.mass = _mass;
+        }
+        public double GetX() { return X; }
+        public double GetY() { return Y; }
+        public double GetVelX() { return VelX; }
+        public double GetVelY() { return VelY; }
+        public double GetR() { return R; }
+        public double GetMass() { return mass; }
+        public void SetX(double x) { X = x; }
+        public void SetY(double y) { Y = y; }
+        public void SetVX(double vx) { VelX = vx; }
+        public void SetVY(double vy) { VelY = vy; }
+        public void SetR(int r) { R = r; }
+
+
+        /*      public void simulateMove(int size)
+              {
+
+                  int newx = X + VelX;
+                  int newy = Y + VelY;
+
+                  if (newx > size || newx < 0)
+                  {
+                      VelX *= -1;
+                  }
+                  if (newy > size || newy < 0)
+                  {
+                      VelY *= -1;
+                  }
+
+                  X += VelX;
+                  Y += VelY;
+              }*/
+        public void Move()
+        {
+            X += VelX;
+            Y += VelY;
+            RaisePropertyChanged("Position");
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
---
+++ Data/Board.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Data
+{
+    public class Board
+    {
+
+        private readonly int size;
+        private readonly List<Ball> balls;
+
+        public Board(int s)
+        {
+            if (s <= 0 )
+                throw new ArgumentException();
+            size = s;
+            balls = new List<Ball>();
+        }
+        //i tak potzrebujemy jako osobne te kulki
+/*        public void AddRandomBalls(int ballsAmount)
+        {
+            Random rand = new Random();
+
+            for (int i = 0; i < ballsAmount; i++)
+            {
+                int r = 5;
+                int m = 1;
+                int x = rand.Next(r, this.size - r);
+                int y = rand.Next(r, this.size - r);
+
+                int vx = rand.Next(1, 5);
+                int vy = rand.Next(1, 5);
+
+                balls.Add(new Ball(x, y, r, vx, vy, m));
+            }
+       
+        public Ball AddRandomBall(int size)
+        {
+            Random rand = new Random();
+
+
+            int r = 5;
+            int m = 1;
+            int x = rand.Next(r, this.size - r);
+            int y = rand.Next(r, this.size - r);
+
+            int vx = rand.Next(1, 15);
+            int vy = rand.Next(1, 15);
+            Ball Bally = new Ball(x, y, r, vx, vy, m);
+            balls.Add(new Ball(x, y, r, vx, vy, m));
+            return Bally;
+        }
+
+      public void MoveBalls()
+        {
+            foreach (Ball ball in balls)
+            {
+                ball.simulateMove(size);
+            }
+        }*/
+
+        public int GetSize() { return size; }
+
+        public List<Ball> GetBalls()
+        {
+            return balls;
+        }
+        public void AddBall(Ball ball)
+        {
+            balls.Add(ball);
+        }
+    }
+}
