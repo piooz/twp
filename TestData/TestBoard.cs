496c37c313687fcccc5b2a6705a2a504609ad4a0
wikslu <wiktoria.sluzewska@gmail.com>
2022-05-26 19:51:21 +0200

    data test again

---
+++ TestData/TestBoard.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Data;
+
+namespace TestData
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            Board board = new Board(1000, 800);
+            Ball ball = new Ball(10, 20, 10, 10);
+            Ball ball2 = new Ball(200, 200, 8.4, 9.2);
+
+            Assert.AreEqual(1000, board.Height);
+            Assert.AreEqual(800, board.Width);
+
+            Assert.ThrowsException<System.ArgumentException>(() => new Board(-100, 200));
+            Assert.ThrowsException<System.ArgumentException>(() => new Board(100, 0));
+
+
+            Assert.AreEqual(0, board.GetBalls().Count);
+
+            board.AddBall(ball);
+            board.AddBall(ball);
+
+            Assert.AreEqual(2, board.GetBalls().Count);
+
+
+        }
+    }
+}
---
+++ TestData/TestDataApi.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Data;
+using System.Collections.Generic;
+
+namespace TestData
+{
+    [TestClass]
+    internal class TestDataApi
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            DataApi dataLayer = DataApi.Create();
+
+            dataLayer.CreateBoard(1000, 800, 3, 3);
+
+            Board board = dataLayer.GetBoard();
+            List<Ball> balls = board.GetBalls();
+
+            Assert.AreEqual(board.Height, 1000);
+            Assert.AreEqual(board.Width, 800);
+            Assert.AreEqual(balls.Count, 3);
+
+            foreach (Ball ball in balls)
+            {
+                Assert.AreEqual(ball.GetR(), 3);
+            }
+
+        }
+    }
+}
